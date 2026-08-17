2c3210bff4e5360d2f9d96e3c3be1f72f4f057ce
dataPikud <data.pikud@gmail.com>
2026-08-17 10:27:10 +0300

    feat: add Jest configuration and integration tests for catalog routes

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,10 +1,9 @@
-export default {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-
   testMatch: ['**/*.test.ts'],
-
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\\.\\.?/.*)\\.js$': '$1',
   },
 };
