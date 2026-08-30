a7439240cabfb01dcfe8333bb29a28c1de322197
Eitan <wandereitan@gmail.com>
2026-08-30 10:12:55 +0300

    fixed type error and email service

--- shared-types/src/index.ts
+++ shared-types/src/index.ts
@@ -1,7 +1,18 @@
 export * from './user.types.js';
 export * from './product.types.js';
-export * from './attribute.types.js';
 export * from './pricing.types.js';
 export * from './cart.types.js';
 export * from './order.types.js';
 export * from './email.types.js';
+export {
+  AttributeType,
+  PricingImpactType,
+  PriceModifierType,
+  AttributeDisplayStyle,
+} from './attribute.types.js';
+
+export type {
+  ProductAttributeOptionDto,
+  CreateAttributeDto,
+  UpdateAttributeDto,
+} from './attribute.types.js';
